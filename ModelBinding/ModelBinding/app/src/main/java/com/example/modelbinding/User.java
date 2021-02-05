ed28d360608b3c631bce70987abb81659e0ad0a6
jdlacy <jdlacy@gmail.com>
2021-02-05 01:22:52 -0800

    working copy

--- ModelBinding/ModelBinding/app/src/main/java/com/example/modelbinding/User.java
+++ ModelBinding/ModelBinding/app/src/main/java/com/example/modelbinding/User.java
@@ -3,11 +3,12 @@ import androidx.lifecycle.MutableLiveData;
 
 public class User {
     public MutableLiveData<String> name;
-    public String email;
+    public MutableLiveData<String> email;
 
     public User(String name, String email) {
         this.name = new MutableLiveData<>();
         this.name.setValue(name);
-        this.email = email;
+        this.email = new MutableLiveData<>();
+        this.email.setValue(email);
     }
 }
