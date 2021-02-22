7af72700d31e1d899ab7f84c311a0504fee7eb2e
jdlacy <jdlacy@gmail.com>
2021-02-21 18:36:41 -0800

    removed UnitConverter directory

--- UnitCoverter/app/src/main/java/com/example/unitcoverter/Converter.java
+++
@@ -1,8 +0,0 @@
-package com.example.unitcoverter;
-
-public class Converter {
-    public static float toCelcius(float fah) {
-        return (fah - 32) * 5/9;
-    }
-    public static float toKiloG(float lbs) {return (lbs * (float)0.45359237); }
-}
